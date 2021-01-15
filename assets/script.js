f8e864a5876d3e7d56af056c00f7a64249032cd6
riraq <riraq@mail.usf.edu>
2021-01-15 16:50:24 -0600

    renamed timer element in html to time; created timer function in js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,29 @@
+// html elements
+var highscoreEl = document.getElementById("viewHighscores");
+var timeEl = document.getElementById("time");
+var quizEl = document.getElementById("quiz");
+var startBtn = document.getElementById("startBtn");
 // array containing questions
 // array containing answers
 
-// function that starts quiz
+// function that starts quiz and timer
+startBtn.addEventListener("click", timerCount);
 
 // function that starts a timer when the start button is clicked and clears when quiz is failed or ends
+function timerCount() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function(){
+        timeEl.textContent = "Time: " + timeLeft;
+        timeLeft--;
+        console.log(timeLeft)
+
+            if (timeLeft === -1) {
+                timeEl.textContent = "Time: 0";
+                clearInterval(timeInterval);
+            };
+    }, 1000);
+};
 // connect timer to quiz questions
 
 // function that shows the high scores by removing other elements and shifts the page back to the quiz when another button is clicked
