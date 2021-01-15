b8543f0a9ba821d9b2b0f73695f1cd06a5183f7b
riraq <riraq@mail.usf.edu>
2021-01-15 15:47:02 -0600

    basic html structure designed, js comments show workflow

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// array containing questions
+// array containing answers
+
+// function that starts quiz
+
+// function that starts a timer when the start button is clicked and clears when quiz is failed or ends
+// connect timer to quiz questions
+
+// function that shows the high scores by removing other elements and shifts the page back to the quiz when another button is clicked
+// function for button that clears high scores
+// function for button that goes back to main menu
+// function that stores high scores into local storage
