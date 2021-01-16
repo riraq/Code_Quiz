9f12aa8040999aa4ab184998be4dbed686b12a72
riraq <riraq@mail.usf.edu>
2021-01-15 20:53:02 -0600

    created function for quiz answers to be created when quiz is started

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,37 @@
-//html elements
+// html elements
 var highscoreEl = document.getElementById("viewHighscores");
 var timeEl = document.getElementById("time");
 var quizEl = document.getElementById("quiz");
 var startBtn = document.getElementById("startBtn");
-//array containing questions
+var quizBtn = document.querySelector(".button");
+// array containing questions
 var questions = ["What is JavaScript?", "What HTML element is used to link to a JavaScript file?", "Which is not used to declare a variable in JavaScript?", "What goes inside the parentheses of a function?", "What symbol is used to by the compiler to separate JavaScript statements?"];
 
-//array containing answers
+// array containing answers
 var answers = ["A programming language", "<script>", "be", "argument", ";"]
 
-//function that starts a timer when the start button is clicked and clears when quiz is failed or ends
+// create buttons for choices on quiz
+var btn1 = document.createElement("button");
+var btn2 = document.createElement("button");
+var btn3 = document.createElement("button");
+var btn4 = document.createElement("button");
+var btnArray = [btn1, btn2, btn3, btn4];
+
+// function that starts a timer when the start button is clicked and clears when quiz is failed or ends
+function quizQuestions(){
+    startBtn.remove();
+    quizEl.children[1].textContent = questions[0];
+    
+    answers1 = ["Programming language", "Sandwich", "Coffee movie script", "Reference book"]
+
+    for (var i=0; i<btnArray.length; i++){
+        quizBtn.appendChild(btnArray[i]);
+        quizBtn.children[i].textContent = answers1[i]
+    };
+    
+};
+
+// starts the timer countdown when the start quiz button is pressed
 function timerCount() {
     var timeLeft = 60;
 
@@ -24,14 +46,15 @@ function timerCount() {
     }, 1000);
 };
 
-//event that starts quiz and timer
+// event that starts quiz and timer
 startBtn.addEventListener("click", function(){
     
     timerCount();
+    quizQuestions();
     }
 );
 
-//function that shows the high scores by removing other elements and shifts the page back to the quiz when another button is clicked
-//function for button that clears high scores
-//function for button that goes back to main menu
-//function that stores high scores into local storage
+// function that shows the high scores by removing other elements and shifts the page back to the quiz when another button is clicked
+// function for button that clears high scores
+// function for button that goes back to main menu
+// function that stores high scores into local storage
